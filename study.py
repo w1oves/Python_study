3ed8168e8e17d7e1ea14a8341cd808c364a03bf8
wzx <18297901734@163.com>
2019-08-30 22:28:00 +0800

    reference

--- study.py
+++ study.py
@@ -1,24 +1,13 @@
 from functools import reduce
-DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,'.':10}
-point=False
-wei=0
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
 def str2float(s):
-    global point
-    global wei
-    wei=0
-    point=False
+    n=s.index('.')
+    without=s.replace(".","")
     def char2num(x):
         return DIGITS[x]
     def fn(x,y):
-        global point
-        global wei
-        if y==10:
-            point=True
-            return x
-        if point:
-            wei+=1
         return x*10+y
-    return (reduce(fn,map(char2num,s))/pow(10,wei))
+    return (reduce(fn,map(char2num,without))/(10**n))
 print('str2float(\'123.456\') =', str2float('123.456'))
 if abs(str2float('123.456') - 123.456) < 0.00001:
     print('测试成功!')
