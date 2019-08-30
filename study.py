21d991c017d936c12d2f331fda9d0fe2bd0f630c
wzx <18297901734@163.com>
2019-08-30 22:11:27 +0800

    map/reduce-3

--- study.py
+++ study.py
@@ -1,10 +1,26 @@
 from functools import reduce
-def prod(L):
-    def mul(x,y):
-        return x*y
-    return reduce(mul,L)
-print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
-if prod([3, 5, 7, 9]) == 945:
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,'.':10}
+point=False
+wei=0
+def str2float(s):
+    global point
+    global wei
+    wei=0
+    point=False
+    def char2num(x):
+        return DIGITS[x]
+    def fn(x,y):
+        global point
+        global wei
+        if y==10:
+            point=True
+            return x
+        if point:
+            wei+=1
+        return x*10+y
+    return (reduce(fn,map(char2num,s))/pow(10,wei))
+print('str2float(\'123.456\') =', str2float('123.456'))
+if abs(str2float('123.456') - 123.456) < 0.00001:
     print('测试成功!')
 else:
     print('测试失败!')
